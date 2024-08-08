1c2e43a3f6b7bdb0b329d0c00cbceb09f464fab2
ColeeneP <75931435+ColeeneP@users.noreply.github.com>
2024-08-08 13:33:17 +0200

    fix responsive on index page

--- index.js
+++
@@ -1,8 +0,0 @@
-// function OpenMenu() {
-//   let nav = document.getElementsById("side_nav_open");
-//     if (nav.style.display === "block") {
-//       nav.style.display = "none";
-//     } else {
-//       nav.style.display = "block";
-//     }
-// }
